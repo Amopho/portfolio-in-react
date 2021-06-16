b4a802301d3fd27d2653a9d47d51bac857ffc75f
Amopho <amopho78@gmail.com>
2021-06-16 14:31:17 +0200

    menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,23 +3,27 @@ import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <nav>
-      <Link to="/">
-        <li>.my-portfolio</li>
-      </Link>
-      <Link to="/">
-        <li>Home</li>
-      </Link>
-      <Link to="/projects">
-        <li>Projects</li>
-      </Link>
-      <Link to="/blog">
-        <li>Blog</li>
-      </Link>
-      <Link to="/contact">
-        <li>Contact</li>
-      </Link>
-    </nav>
+    <header>
+      <nav>
+        <Link class="logo" to="/">
+          <li>.my-portfolio</li>
+        </Link>
+        <div class="menu">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/projects">
+            <li>Projects</li>
+          </Link>
+          <Link to="/blog">
+            <li>Blog</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 };
 
