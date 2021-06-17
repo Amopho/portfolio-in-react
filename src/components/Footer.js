f9ff17297ef8f7b96b84df4eaf7473ae66cd03ec
Amopho <amopho78@gmail.com>
2021-06-17 14:34:07 +0200

    footer first link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,70 +6,23 @@ const Footer = () => {
   let year = new Date().getFullYear();
   return (
     <Router>
-    <div class="footer">
-      <div class="social-links">
-        <a href="#scroll" class="arrow-button">
-          <span>
-            <FiArrowDownCircle />
-          </span>
-        </a>
-      </div>
-      <div class="social-links">
-        <a href="#scroll" class="arrow-button">
-          <span>
-            <FiArrowDownCircle />
-          </span>
-        </a>
-      </div>
-      <div class="social-links">
-        <a href="#scroll" class="arrow-button">
-          <span>
-            <FiArrowDownCircle />
-          </span>
-        </a>
-      </div>
-      <div class="social-links">
-        <a href="#scroll" class="arrow-button">
-          <span>
-            <FiArrowDownCircle />
-          </span>
-        </a>
-      </div>
-    </div>
-
-      <div class="social-links">
-        <ul>
-          <li>
-            <div class="social">
-              <a href="#"
-                ><span><i class="fab fa-linkedin"></i></span
-              ></a>
-            </div>
-          </li>
-          <li>
-            <div class="social">
-              <a href="#"
-                ><span><i class="fab fa-github-square"></i></span
-              ></a>
-            </div>
-          </li>
-          <li>
-            <div class="social">
-              <a href="#"
-                ><span><i class="fab fa-researchgate"></i></span
-              ></a>
-            </div>
-          </li>
-          <li>
-            <div class="social">
-              <a href="#"
-                ><span><i class="fab fa-twitter-square"></i></i></span></a>
-            </div>
-          </li>
-        </ul>
-
-      <div>
-        <h4>Copyrights ®{year} </h4>
+      <div href="#footer" class="footer">
+        <div class="social-links">
+          <ul>
+            <li>
+              <div class="social">
+                <a href="#">
+                  <span>
+                    <FiArrowDownCircle />
+                  </span>
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>Copyrights ®{year} </h4>
+        </div>
       </div>
     </Router>
   );
