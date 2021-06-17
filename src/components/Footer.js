2370d54001ccb8c364d3350d1e715a90c2cc0c4f
Amopho <amopho78@gmail.com>
2021-06-17 13:41:26 +0200

    footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,43 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { FiArrowDownCircle } from "react-icons/fi";
+
 const Footer = () => {
   let year = new Date().getFullYear();
   return (
-    <div>
-      <h4>Copyrights ®{year} </h4>
-    </div>
+    <Router>
+      <div class="go-down">
+        <a href="#scroll" class="arrow-button">
+          <span>
+            <FiArrowDownCircle />
+          </span>
+        </a>
+      </div>
+      <div class="go-down">
+        <a href="#scroll" class="arrow-button">
+          <span>
+            <FiArrowDownCircle />
+          </span>
+        </a>
+      </div>
+      <div class="go-down">
+        <a href="#scroll" class="arrow-button">
+          <span>
+            <FiArrowDownCircle />
+          </span>
+        </a>
+      </div>
+      <div class="go-down">
+        <a href="#scroll" class="arrow-button">
+          <span>
+            <FiArrowDownCircle />
+          </span>
+        </a>
+      </div>
+      <div>
+        <h4>Copyrights ®{year} </h4>
+      </div>
+    </Router>
   );
 };
 
