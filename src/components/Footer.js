7ba8a246375d23ae0e05b3ec9ac6df66995e8a69
Amopho <amopho78@gmail.com>
2021-06-17 14:16:15 +0200

    tags for social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,34 +6,68 @@ const Footer = () => {
   let year = new Date().getFullYear();
   return (
     <Router>
-      <div class="go-down">
+    <div class="footer">
+      <div class="social-links">
         <a href="#scroll" class="arrow-button">
           <span>
             <FiArrowDownCircle />
           </span>
         </a>
       </div>
-      <div class="go-down">
+      <div class="social-links">
         <a href="#scroll" class="arrow-button">
           <span>
             <FiArrowDownCircle />
           </span>
         </a>
       </div>
-      <div class="go-down">
+      <div class="social-links">
         <a href="#scroll" class="arrow-button">
           <span>
             <FiArrowDownCircle />
           </span>
         </a>
       </div>
-      <div class="go-down">
+      <div class="social-links">
         <a href="#scroll" class="arrow-button">
           <span>
             <FiArrowDownCircle />
           </span>
         </a>
       </div>
+    </div>
+
+      <div class="social-links">
+        <ul>
+          <li>
+            <div class="social">
+              <a href="#"
+                ><span><i class="fab fa-linkedin"></i></span
+              ></a>
+            </div>
+          </li>
+          <li>
+            <div class="social">
+              <a href="#"
+                ><span><i class="fab fa-github-square"></i></span
+              ></a>
+            </div>
+          </li>
+          <li>
+            <div class="social">
+              <a href="#"
+                ><span><i class="fab fa-researchgate"></i></span
+              ></a>
+            </div>
+          </li>
+          <li>
+            <div class="social">
+              <a href="#"
+                ><span><i class="fab fa-twitter-square"></i></i></span></a>
+            </div>
+          </li>
+        </ul>
+
       <div>
         <h4>Copyrights ®{year} </h4>
       </div>
