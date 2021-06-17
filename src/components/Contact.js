d8c82a0c16ef84a76ea2503aebcc7d9f647392ea
Amopho <amopho78@gmail.com>
2021-06-17 15:19:12 +0200

    contact text content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,33 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 const Contact = () => {
-  return <h1>Contact</h1>;
+  return (
+    <Router>
+      <h1>Contact</h1>
+      <div class="sections-container">
+        <div class="section-container">
+          <div class="hi-img">
+            <img src="./img/pink-glasses.jpg" alt="black glasses" />
+          </div>
+          <div class="section-text">
+            <div class="text">
+              <p>Dr. Alina Majewska-Pinda</p>
+              <br />
+              <p>A Ph.D in Animal Science and Husbandry</p>
+              <p>alina.majewska.pinda@gmail.com</p>
+              <br />
+              <h1>I would like to hear from you.</h1>
+            </div>
+            <div class="section-button">
+              <button>
+                <a href="./contact"> Let's get to know us</a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 };
 
 export default Contact;
