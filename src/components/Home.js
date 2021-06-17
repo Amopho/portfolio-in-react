4ec3b5c6633f4ae30b5d486d68d634c1c84bb852
Amopho <amopho78@gmail.com>
2021-06-17 10:38:26 +0200

    image import

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Hero from "../../public/img/pink-glasses.jpg";
+// import Img from "../public/img/pink-glasses.jpg";
 
 const Home = () => {
+  console.log(process.env);
   return (
     <Router>
       <h1>Hi, I am Alina!</h1>
-      {/* <img src={Hero} alt="pink glasses" />; */}
+      <img
+        src={process.env.PUBLIC_URL + "/img/pink-glasses.jpg"}
+        alt="pink glasses"
+      />
+      ;
     </Router>
   );
 };
